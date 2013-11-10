7c029326932ffbefb3888a9c02616a41129cd0fc
unknown <gaumala@espol.edu.ec>
2013-11-10 18:33:38 -0500

    Interfaz Grafica Comenzada  Se creó un nuevo branch con la interfaz gráfica para buscaminas. La primera actividad tiene botones donde el usuario selecciona la dificultad que desea: fácil, normal, o difícil. Cada botón crea una actividad distinta y propia del nivel de dificultad con todas sus caracteristicas. El tablero se crea a partir de un arreglo que contiene todas las celdas usando un GridView y adaptadores. Los tableros fácil, normal y dificil son clases que heredan de la clase ImageAdapter que hereda de BaseAdapter, clase que permite llevar los datos del arreglo al GridView. Además del Tablero se colocó el botón de Reset, que al ser pulsado crea una nueva actividad con un tablero de la misma dificultad pero completamente nuevo. También se añadió soporte para dispositivos HDPI (Alta resolución) y MDPI (Resolución media). Esto fue verificado en un Galaxy Player 4.0 y una Galaxy Tab 3.0. Para cada resolución se crearon imágenes nuevas

---
+++ Practica1/src/com/example/practica1/GridDemo.java
@@ -0,0 +1,73 @@
+package com.example.practica1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+public class GridDemo extends Activity {
+
+	GridView gridview;
+	ImageAdapter tablero;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_grid_demo);
+		tablero = new TableroFacil(this, showTheMetrics());
+		gridview = (GridView) findViewById(R.id.gridview);
+		
+	    gridview.setAdapter(tablero);
+	    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	    
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            //Toast.makeText(GridDemo.this, "" + position, Toast.LENGTH_SHORT).show();
+	            tablero.changeImgAfterClicked(position);
+	            gridview.invalidateViews();
+	            
+	        }
+
+	    });
+	    
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.grid_demo, menu);
+		return true;
+	}
+	
+	public int showTheMetrics(){
+		 int density= getResources().getDisplayMetrics().densityDpi;
+
+		 switch(density)
+		 {
+		 case DisplayMetrics.DENSITY_LOW:
+		    return 36;
+		case DisplayMetrics.DENSITY_MEDIUM:
+		      return 24;
+		 case DisplayMetrics.DENSITY_HIGH:
+		     return 36;
+		 case DisplayMetrics.DENSITY_XHIGH:
+		      return 36;
+		     
+		 }
+		 
+		 return 24;
+	}
+	
+	public void resetGame(View view){
+		Intent intent = new Intent(this, GridDemo.class);
+		startActivity(intent);
+		finish();
+	}
+
+}
